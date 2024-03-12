37bbfcf2d71bf912e7d4157fed9dcc8092be7939
hong630 <hongihongi60@gmail.com>
2024-03-12 17:47:30 +0900

    [0312] nest main page

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 정적 파일 제공을 위한 디렉토리 설정
+app.use(express.static('public'));
+
+// 루트 경로로 접속했을 때 home.html 파일을 보내기
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/html/home.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
