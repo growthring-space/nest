c27b9594fa4dab81c19add04b536a8138b221eeb
ryouJay <ryoujaydev@gmail.com>
2024-03-12 19:06:34 +0900

    [20240312] optimize to github Pages

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const port = 3000;
 // 정적 파일 제공을 위한 디렉토리 설정
 app.use(express.static('public'));
 
-// 루트 경로로 접속했을 때 home.html 파일을 보내기
+// 루트 경로로 접속했을 때 index.html 파일을 보내기
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/html/home.html');
+    res.sendFile(__dirname + '/public/html/index.html');
 });
 
 app.listen(port, () => {
